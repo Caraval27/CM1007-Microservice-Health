263e32c5cd80543fa388eb1867f741ab8cd6fa41
Caraval27 <bettypori@gmail.com>
2024-12-18 22:41:32 +0100

    Junit test test fix

--- Lab3_Health/src/test/java/journal/lab3_health/Lab3HealthApplicationTests.java
+++ Lab3_Health/src/test/java/journal/lab3_health/Lab3HealthApplicationTests.java
@@ -1,12 +1,20 @@
 package journal.lab3_health;
 
+import journal.lab3_health.Core.HealthService;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Lab3HealthApplicationTests {
 
+	@Autowired
+	private HealthService healthService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(healthService, "HealthService bean should be loaded");
 	}
 }
